93dbd866d5f90efb1791c3d2c1e7bdd5024dd3d4
Leo Home <realleo2009@hotmail.com>
2024-10-25 14:41:49 -0300

    password manager v1.4

--- lib/modules/password_manager/view/password_manager_screen.dart
+++ lib/modules/password_manager/view/password_manager_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:utilitarios/modules/password_manager/cubit/password_manager_cubit.dart';
 
@@ -6,6 +7,7 @@ class PasswordManagerScreen extends StatelessWidget {
   final _nameController = TextEditingController();
   final _descriptionController = TextEditingController();
   final _passwordController = TextEditingController();
+  final _formKey = GlobalKey<FormState>(); // Key para o formulário
 
   PasswordManagerScreen({super.key});
 
@@ -13,167 +15,241 @@ class PasswordManagerScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text('Gerenciador de Senhas')),
-      body: BlocBuilder<PasswordManagerCubit, PasswordManagerState>(
-        builder: (context, state) {
-          if (state.status == PasswordManagerStatus.loading) {
-            return Center(child: CircularProgressIndicator());
-          } else if (state.status == PasswordManagerStatus.loaded) {
-            return ListView.builder(
-              itemCount: state.passwords.length,
-              itemBuilder: (context, index) {
-                final password = state.passwords[index];
-                return Card(
-                  elevation: 3, // Adiciona uma leve sombra ao card
-                  margin: EdgeInsets.symmetric(vertical: 10, horizontal: 16),
-                  shape: RoundedRectangleBorder(
-                    borderRadius:
-                        BorderRadius.circular(10), // Bordas arredondadas
-                  ),
-                  child: InkWell(
-                    onLongPress: () {
-                      // Ao segurar o card, abrir o diálogo para confirmação de exclusão
-                      showDialog(
-                        context: context,
-                        builder: (_) {
-                          return AlertDialog(
-                            title: Text('Excluir Senha'),
-                            content: Text(
-                                'Você tem certeza que deseja excluir a senha "${password['name']}"?'),
-                            actions: [
-                              TextButton(
-                                onPressed: () {
-                                  Navigator.of(context)
-                                      .pop(); // Fechar o diálogo
-                                },
-                                child: Text('Cancelar'),
+      body: Stack(
+        children: [
+          BlocBuilder<PasswordManagerCubit, PasswordManagerState>(
+            builder: (context, state) {
+              if (state.status == PasswordManagerStatus.loading) {
+                return Center(child: CircularProgressIndicator());
+              } else if (state.status == PasswordManagerStatus.loaded) {
+                return ListView.builder(
+                  itemCount: state.passwords.length + 1,
+                  itemBuilder: (context, index) {
+                    if (index == state.passwords.length) {
+                      return SizedBox(height: 70);
+                    }
+
+                    final password = state.passwords[index];
+                    return Card(
+                      elevation: 3,
+                      margin:
+                          EdgeInsets.symmetric(vertical: 10, horizontal: 16),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      child: InkWell(
+                        onTap: () {
+                          dialog(context, password);
+                        },
+                        child: Padding(
+                          padding: const EdgeInsets.all(16.0),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                password['name'],
+                                style: TextStyle(
+                                  fontSize: 18,
+                                  fontWeight: FontWeight.bold,
+                                ),
                               ),
-                              TextButton(
-                                onPressed: () {
-                                  // Excluir a senha
+                              SizedBox(height: 10),
+                              Text('Descrição: ${password['description']}'),
+                              SizedBox(height: 10),
+                            ],
+                          ),
+                        ),
+                      ),
+                    );
+                  },
+                );
+              } else if (state.status == PasswordManagerStatus.adding) {
+                return Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: Form(
+                    key: _formKey, // Atribui o GlobalKey ao Form
+                    child: SingleChildScrollView(
+                      physics: const BouncingScrollPhysics(),
+                      child: Column(
+                        children: [
+                          TextFormField(
+                            controller: _nameController,
+                            maxLength: 50,
+                            decoration: InputDecoration(
+                              labelText: 'Nome',
+                              border: OutlineInputBorder(),
+                            ),
+                            inputFormatters: [
+                              LengthLimitingTextInputFormatter(256),
+                            ],
+                            validator: (value) {
+                              if (value == null || value.isEmpty) {
+                                return 'O campo Nome é obrigatório';
+                              }
+                              return null;
+                            },
+                          ),
+                          SizedBox(height: 16),
+                          TextFormField(
+                            controller: _descriptionController,
+                            maxLength: 50,
+                            decoration: InputDecoration(
+                              labelText: 'Descrição',
+                              border: OutlineInputBorder(),
+                            ),
+                            inputFormatters: [
+                              LengthLimitingTextInputFormatter(256),
+                            ],
+                            validator: (value) {
+                              if (value == null || value.isEmpty) {
+                                return 'O campo Descrição é obrigatório';
+                              }
+                              return null;
+                            },
+                          ),
+                          SizedBox(height: 16),
+                          TextFormField(
+                            controller: _passwordController,
+                            maxLength: 256,
+                            obscureText: true,
+                            decoration: InputDecoration(
+                              labelText: 'Senha',
+                              border: OutlineInputBorder(),
+                            ),
+                            inputFormatters: [
+                              LengthLimitingTextInputFormatter(256),
+                            ],
+                            validator: (value) {
+                              if (value == null || value.isEmpty) {
+                                return 'O campo Senha é obrigatório';
+                              }
+                              return null;
+                            },
+                          ),
+                          SizedBox(height: 20),
+                          ElevatedButton(
+                            onPressed: () {
+                              if (_formKey.currentState!.validate()) {
+                                final name = _nameController.text;
+                                final description = _descriptionController.text;
+                                final password = _passwordController.text;
+
+                                if (name.isNotEmpty &&
+                                    description.isNotEmpty &&
+                                    password.isNotEmpty) {
                                   context
                                       .read<PasswordManagerCubit>()
-                                      .deletePassword(password[
-                                          'id']); // Exemplo: usando o id da senha
-                                  Navigator.of(context)
-                                      .pop(); // Fechar o diálogo após a exclusão
-                                },
-                                child: Text('Excluir',
-                                    style: TextStyle(color: Colors.red)),
-                              ),
-                            ],
-                          );
-                        },
-                      );
-                    },
-                    onTap: () {
-                      showDialog(
-                        context: context,
-                        builder: (BuildContext context) {
-                          return AlertDialog(
-                            title: Text(password['name']),
-                            content: Column(
-                              mainAxisSize: MainAxisSize.min,
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                Text('Descrição: ${password['description']}'),
-                                SizedBox(height: 10),
-                                Text(
-                                    'Senha: ${password['password']}'), // Senha descriptografada
-                              ],
-                            ),
-                            actions: [
-                              TextButton(
-                                onPressed: () {
-                                  Navigator.of(context).pop();
-                                },
-                                child: Text('Fechar'),
-                              ),
-                            ],
-                          );
-                        },
-                      );
-                    },
-                    child: Padding(
-                      padding: const EdgeInsets.all(16.0),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text(
-                            password['name'],
-                            style: TextStyle(
-                              fontSize: 18,
-                              fontWeight: FontWeight.bold,
-                            ),
+                                      .addPassword(
+                                        name,
+                                        password,
+                                        description,
+                                      );
+
+                                  _nameController.clear();
+                                  _passwordController.clear();
+                                  _descriptionController.clear();
+                                }
+                              }
+                            },
+                            child: Text('Salvar Senha'),
                           ),
-                          SizedBox(height: 10),
-                          Text('Descrição: ${password['description']}'),
-                          SizedBox(height: 10),
+                          SizedBox(height: 16),
+                          ElevatedButton(
+                              child: Text('Voltar'),
+                              onPressed: () {
+                                context
+                                    .read<PasswordManagerCubit>()
+                                    .loadPassword();
+                              }),
                         ],
                       ),
                     ),
                   ),
                 );
-              },
-            );
-          } else if (state.status == PasswordManagerStatus.adding) {
-            return Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: Column(
-                children: [
-                  TextField(
-                    controller: _nameController,
-                    decoration: InputDecoration(labelText: 'Nome'),
-                  ),
-                  TextField(
-                    controller: _descriptionController,
-                    decoration: InputDecoration(labelText: 'Descrição'),
-                  ),
-                  TextField(
-                    controller: _passwordController,
-                    decoration: InputDecoration(labelText: 'Senha'),
-                    obscureText: true, // Para esconder a senha
-                  ),
-                  SizedBox(height: 20),
-                  ElevatedButton(
-                    onPressed: () {
-                      final name = _nameController.text;
-                      final description = _descriptionController.text;
-                      final password = _passwordController.text;
-
-                      if (name.isNotEmpty &&
-                          description.isNotEmpty &&
-                          password.isNotEmpty) {
-                        context.read<PasswordManagerCubit>().addPassword(
-                              name,
-                              password,
-                              description,
-                            );
-
-                        // Limpa os campos do formulário após salvar
-                        _nameController.clear();
-                        _passwordController.clear();
-                        _descriptionController.clear();
-                      }
-                    },
-                    child: Text('Salvar Senha'),
-                  ),
-                ],
+              } else if (state.status == PasswordManagerStatus.error) {
+                return Center(child: Text('Erro: ${state.errorMessage}'));
+              } else {
+                return Center(child: Text('Nenhuma senha encontrada.'));
+              }
+            },
+          ),
+          if (context.watch<PasswordManagerCubit>().state.status ==
+              PasswordManagerStatus.loaded)
+            Positioned(
+              bottom: 16,
+              left: 16,
+              right: 16,
+              child: ElevatedButton(
+                onPressed: () {
+                  context.read<PasswordManagerCubit>().showAddPasswordForm();
+                },
+                child: Text('Adicionar Senha'),
               ),
-            );
-          } else if (state.status == PasswordManagerStatus.error) {
-            return Center(child: Text('Erro: ${state.errorMessage}'));
-          } else {
-            return Center(child: Text('Nenhuma senha encontrada.'));
-          }
-        },
+            ),
+        ],
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          // Alterna para o modo de adicionar senha
-          context.read<PasswordManagerCubit>().showAddPasswordForm();
-        },
-        child: Icon(Icons.add),
-      ),
+    );
+  }
+
+  Future<dynamic> dialog(BuildContext context, Map<String, dynamic> password) {
+    return showDialog(
+      context: context,
+      builder: (_) {
+        return AlertDialog(
+          title: Text(password['name']),
+          content: Column(
+            mainAxisSize: MainAxisSize.min,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Text('Descrição: ${password['description']}'),
+              SizedBox(height: 10),
+              Text('Senha: ${password['password']}'),
+            ],
+          ),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+                showDialog(
+                  context: context,
+                  builder: (_) {
+                    return AlertDialog(
+                      title: Text('Excluir Senha'),
+                      content: Text(
+                          'Você tem certeza que deseja excluir a senha "${password['name']}"?'),
+                      actions: [
+                        TextButton(
+                          onPressed: () {
+                            Navigator.of(context).pop();
+                          },
+                          child: Text('Cancelar'),
+                        ),
+                        TextButton(
+                          onPressed: () {
+                            context
+                                .read<PasswordManagerCubit>()
+                                .deletePassword(password['id']);
+                            Navigator.of(context).pop();
+                          },
+                          child: Text('Excluir',
+                              style: TextStyle(color: Colors.red)),
+                        ),
+                      ],
+                    );
+                  },
+                );
+              },
+              child: Text('Deletar', style: TextStyle(color: Colors.red)),
+            ),
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+              child: Text('Fechar'),
+            ),
+          ],
+        );
+      },
     );
   }
 }
